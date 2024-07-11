0dec3cac8f20e4c1da53c634a05362a38e26272b
Shubham <25881429+shoebham@users.noreply.github.com>
2024-07-11 13:06:51 +0530

    add to gitignore

--- stanford-parser-full-2018-10-17/ParserDemo2.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:be72c43ba77ad6ebd53b62dd7d0a0ac70238adcc296eb8d86c2b50f3fc59e765
-size 3625
--- stanford-parser-full-2018-10-17/ShiftReduceDemo.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:763f238835fe4d92c4243fb294a3e0bb12096c392e6ddb4a703ffd561038e87a
-size 1828
